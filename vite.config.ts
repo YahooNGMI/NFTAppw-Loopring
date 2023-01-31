13b6422662bca5d1446d29b102a42e93a9e16832
JohnWRalph <John.Ralph.Git@gmail.com>
2023-01-30 21:42:44 -0600

    readded Loopring

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import nodePolyfills from "vite-plugin-node-stdlib-browser";
-
+import { Buffer } from 'buffer';
 
  
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(),nodePolyfills()],
+  plugins: [svelte(),nodePolyfills(),Buffer],
   define: {global: 'window','process.env.ANCHOR_BROWSER': true,}, 
   resolve: {
     alias: {
