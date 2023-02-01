2bb5ecb583fd1bd39d54bc046cd359798db175e3
JohnWRalph <John.Ralph.Git@gmail.com>
2023-02-01 15:18:41 -0600

    buffer

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,8 @@ import NfTview from "./components/NFTview.svelte";
 import {ethereumAccount, solanaAccount} from "./store/account";
 
   import NftModal from "./components/NFTModal.svelte";
-  
+    import { onMount } from "svelte";
+  import { Buffer } from "buffer";
     
 let visible=true;
 
@@ -19,6 +20,10 @@ function swoop() {
   }
 }
 let showing = true
+
+	onMount(async () => {
+    console.log("Buffer:",Buffer)
+	});
 </script>
 
 
